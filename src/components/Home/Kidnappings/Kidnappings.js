1080583bee358c8bb640a45514420a81e7317917
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2020-04-08 15:40:31 +0200

    :construction: Set kidnapping state

--- src/components/Home/Kidnappings/Kidnappings.js
+++ src/components/Home/Kidnappings/Kidnappings.js
@@ -8,26 +8,32 @@ import Environment from 'environment';
  * @url https://db.nomics.world/Eurostat/crim_off_cat?q=kidnapping
  */
 export default class Kidnappings extends React.Component {
+  state = {
+    frequency: '',
+    hasError: false,
+    isLoading: true,
+    data: []
+  };
+
+  /**
+   * @brief Get data for the component when created
+   */
   componentDidMount() {
     fetch(Environment.dbNomicsUrl + 'v22/series/Eurostat/crim_off_cat?limit=1000&offset=0&q=kidnapping&observations=1&align_periods=1&dimensions=%7B%7D',
       { method: 'GET' })
       .then(res => { return res.json() })
       .then(json => {
-        // console.log(json.dataset.dimensions_values_labels.FREQ.A); // Frequency
         const data = json.series.docs
           .filter(country => country.dimensions.unit !== 'P_HTHAB')
           .map(country => ({
             'country': json.dataset.dimensions_values_labels.geo[country.dimensions.geo],
             'kidnappings': country.period.map((date, index) => ({ 'date': date, 'value': country.value[index] }))
           }));
-  
-        console.log(data);
-        // this.setState({ name: json[0].firstname })
+        this.setState({ frequency: json.series.docs[0]['@frequency'], hasError: false, isLoading: false, data: data });
       })
       .catch(err => {
-        // TODO
-        // this.setState({ name: `failed to get data from : ${Environment.testUrl}`  })
-        console.error(`[Kidnappings] Cannot get  ${Environment.dbNomicsUrl}`);
+        this.setState({ hasError: true, isLoading: false });
+        console.error(`[Kidnappings] Cannot get  ${Environment.dbNomicsUrl} : ${err}`);
       });
   }
 
@@ -35,6 +41,8 @@ export default class Kidnappings extends React.Component {
     return (
       <div className="Kidnappings">
         <p>Kidnappings</p>
+        { this.state.isLoading && <p>Component is loading</p> }
+        <p>{ this.state.data.length }</p>
       </div>
     )
   }
