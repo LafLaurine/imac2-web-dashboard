88de9ea2c72e52f4fb9c0550e16eaeff2d60d1e8
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2020-04-16 19:15:48 +0200

    :lipstick: Hell's dooor

--- src/components/HellsDoor/HellsDoor.js
+++ src/components/HellsDoor/HellsDoor.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './HellsDoor.css';
 
-import chaosDashboard from './img/chaosDashboard.png'
+import chaosDashboard from './img/chaos-dashboard.png'
 import elmo from './img/elmo.gif'
 import hell from './img/hell.gif'
 import satan from './img/satan.gif'
-import gatesOfHell from './img/gatesOfHell.gif'
+import gatesOfHell from './img/gates-of-hell.gif'
 
 export default class HellsDoor extends React.Component {
   render() {
     return (
       <div className="HellsDoor">
-        <img className="wordart" src={chaosDashboard} alt="chaosDashboard wordart"/>
-        <img className="gif" src={elmo} alt="elmo gif" />
-        <img className="gif" src={hell} alt="hell gif" />
-        <img className="gif" src={gatesOfHell} alt="gatesOfHell gif" />
-        <img className="gif" src={satan} alt="satan gif" />
+        <div className="wordart">
+          <img src={chaosDashboard} alt="chaosDashboard wordart"/>
+        </div>
+        <div className="gifs">
+          <img src={elmo} alt="elmo gif" />
+          <img src={hell} alt="hell gif" />
+          <img src={gatesOfHell} alt="gatesOfHell gif" />
+          <img src={satan} alt="satan gif" />
+        </div>
       </div >
     )
   }
