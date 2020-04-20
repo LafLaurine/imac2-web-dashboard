f324f290969a27467c1e65cd88d99cc95484e5ca
Mathilde <mathildedumoulin54@gmail.com>
2020-04-20 14:51:04 +0200

    :sparkles: Crimes : random Country list + change countries button

--- src/components/Statishlag/Crimes/Crimes.js
+++ src/components/Statishlag/Crimes/Crimes.js
@@ -4,14 +4,17 @@ import './Crimes.css';
 import NextButton from './NextButton';
 import Environment from 'environment';
 
-var indexCountry = 0 ;
+let indexCountry = 0 ;
+let indexCountry2 = 1 ;
+let indexCountry3 = 2 ;
+let indexCountry4 = 3 ;
+let selectedIndex = indexCountry;
 
 const step = {
   LOADING: 'loading',
   ERROR: 'error',
   LOADED: 'loaded'
 };
-
 
 export default class Crimes extends React.Component {
   constructor(props) {
@@ -21,13 +24,32 @@ export default class Crimes extends React.Component {
       step: step.LOADING,
       data: []
     }
-    this.updateCountry = this.updateCountry.bind(this)
+    this.updateCountry = this.updateCountry.bind(this);
+    this.updateCrimesSection = this.updateCrimesSection.bind(this);
+
   }
 
-updateCountry() {
+randomizeIndexes(){
 	indexCountry = (Math.floor(Math.random()*this.state.data.length));
+	indexCountry2 = (Math.floor(Math.random()*this.state.data.length));
+	indexCountry3 = (Math.floor(Math.random()*this.state.data.length));
+	indexCountry4 = (Math.floor(Math.random()*this.state.data.length));
+}
+
+updateCountry() {
+	this.randomizeIndexes();
+	while(indexCountry === indexCountry2 || indexCountry2 === indexCountry3 || indexCountry3 === indexCountry4){
+		this.randomizeIndexes();
+	}
+	selectedIndex = indexCountry;
   	this.retrieveData();
 }
+
+updateCrimesSection(myIndex){
+	selectedIndex = myIndex;
+	console.log(myIndex);
+}
+
 
 retrieveData() {
     fetch(Environment.dbNomicsUrl + 'v22/series/Eurostat/crim_gen?limit=1000&offset=0&q=homicide&observations=1&align_periods=1&dimensions={}',
@@ -51,6 +73,8 @@ componentDidMount() {
 	this.retrieveData();
 }
 
+
+
 render() {   
     return (
       <div className="Crimes">
@@ -58,13 +82,24 @@ render() {
         switch(this.state.step) {
           case step.LOADING: return <p>Loading</p>
           case step.LOADED: return (
-            <div>
-	            <h2>2000's CRIMES</h2>
-	            <div id = "contentBlood">
-	            	<h3>{this.state.data[indexCountry].country}</h3>
-	            	<p>{this.state.data[indexCountry].homicides[7].value} homicides</p>
+          	<div>
+	          	<h2>2000's CRIMES</h2>
+	            <div id = "container">
+		            
+		            <div id = "selectCountry">
+		            	<NextButton name = {this.state.data[indexCountry].country}></NextButton>
+		            	<NextButton name = {this.state.data[indexCountry2].country}></NextButton>
+		            	<NextButton name = {this.state.data[indexCountry3].country}></NextButton>
+		            	<NextButton name = {this.state.data[indexCountry4].country}></NextButton>
+		            	<NextButton onClick={e => this.updateCountry()} name="Change countries"></NextButton>
+		            </div>
+		            <div id = "contentBlood">
+		            	<h3>{this.state.data[selectedIndex].country}</h3>
+		            	<p>{this.state.data[selectedIndex].homicides[7].value} homicides</p>
+		            	
+		            </div>
+		            
 	            </div>
-	            <NextButton onClick={e => this.updateCountry()} name="Another country"></NextButton>
             </div>
           )
           default: return <p>Error loading crimes</p>
