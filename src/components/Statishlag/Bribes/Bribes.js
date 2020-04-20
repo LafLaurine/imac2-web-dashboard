cfe6587c71fc310b00b1877917c80b5eb585f159
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2020-04-21 00:51:19 +0200

    :bug: Fix memory leak when component unmounted before request end

--- src/components/Statishlag/Bribes/Bribes.js
+++ src/components/Statishlag/Bribes/Bribes.js
@@ -18,30 +18,40 @@ export default class Bribes extends React.Component {
     this.retrieveData();
   }
 
+  componentWillUnmount() {
+    if (this.state.step === Step.LOADING) 
+      this.controller.abort();
+  }
+
   retrieveData() {
     fetch(Environment.dbNomicsUrl + 'v22/series/WB/WDI?limit=1000&offset=0&q=bribe&observations=1&align_periods=1&dimensions=%7B%7D',
-      { method: 'GET' })
+      { method: 'GET', signal: this.controller.signal })
       .then(res => { return res.json() })
       .then(json => {
-        const data = json.series.docs
-          .map(country => ({
-              'country': json.dataset.dimensions_values_labels.country[country.dimensions.country],
-              'bribes': country.period
-                .map((date, index) => ({ 'date': date, 'value': country.value[index] }))
-                .filter(bribe => bribe.value !== 'NA')
-            })
-          );
+        const data = json.series.docs.map(country => ({
+          'country': json.dataset.dimensions_values_labels.country[country.dimensions.country],
+          'bribes': country.period
+            .map((date, index) => ({ 'date': date, 'value': country.value[index] }))
+            .filter(bribe => bribe.value !== 'NA')
+        }));
 
         this.setState({ frequency: json.series.docs[0]['@frequency'], step: Step.LOADED, data: data });
       })
       .catch(err => {
-        this.setState({ step: Step.ERROR })
-        console.error(`[Bribes] Cannot get  ${Environment.dbNomicsUrl} : ${err}`)
+        if (err.name !== 'AbortError')
+          console.error(`[Bribes] Cannot get  ${Environment.dbNomicsUrl} : ${err}`)
       });
   }
 
   render() {
-    return (
+    switch(this.state.step) {
+    case Step.LOADING: return (
+      <div className="Bribes">
+        <p>Loading</p>
+      </div>
+    )
+
+    case Step.LOADED: return (
       <div className="Bribes">
         <p>Mon texte</p>
         <button>Truc 1</button>
@@ -50,5 +60,13 @@ export default class Bribes extends React.Component {
         <img className="death" src={Death} alt="death" />
       </div>
     )
+
+    default: return (
+      <div className="Bribes">
+        <p>Error</p>
+      </div>
+    )}
   }
+
+  controller = new AbortController();
 }
