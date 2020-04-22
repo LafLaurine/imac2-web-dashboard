bfc4afc2a2e48e3d1550d752fafa8cc45947c39e
Mathilde <mathildedumoulin54@gmail.com>
2020-04-22 15:08:06 +0200

    :poop: First try for tier list elements displaying (absolutely disgusting code)
    Need to be improved, don't watch the code unless you wanna go blind

--- src/components/Statishlag/CausesOfDeath/CausesOfDeath.js
+++ src/components/Statishlag/CausesOfDeath/CausesOfDeath.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import './CausesOfDeath.css';
+import CountryButton from '../CountryButton/CountryButton';
 
 import Environment from 'environment';
 import Step from 'shared/Step';
+
+let arrayTierList = {
+  0: { country : "Lithuania"},
+  1: { country : "Romania"},
+  2: { country : "Estonia"},
+  3: { country : "Portugal"},
+  4: { country : "Latvia"}
+};
+
+let countryTierList;
 
 export default class CausesOfDeath extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      frequency: '',
       step: Step.LOADING,
-      geo: 'AT',
-      data: []
-    };
+      data: [],
+      year : 2014,
+      countries : ['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'EL', 'FR', 'DE', 'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'ES', 'SE'],
+      frequency: '',
+      example : "Lithuania"
+    }
+    this.setYear = this.setYear.bind(this)
+    this.organizeTierList = this.organizeTierList.bind(this)
+  }
+
+  componentWillUnmount() {
+    if (this.state.step === Step.LOADING)
+      this.requestController.abort();
   }
 
   componentDidMount() {
@@ -21,12 +41,13 @@ export default class CausesOfDeath extends React.Component {
       .then(res => { return res.json() })
       .then(json => {
         const data = json.series.docs
-          .filter(geo => geo.dimensions.geo === this.state.geo)
+          .filter(geo => this.state.countries.includes(geo.dimensions.geo))
+          .filter(values => values.value[0] < 4)
           .map(country => ({
             'country': json.dataset.dimensions_values_labels.geo[country.dimensions.geo],
             'deaths': country.period.map((date, index) => ({ 'date': date, 'value': country.value[index] })),
           }))
-        this.setState({ frequency: json.series.docs[0]['@frequency'], step: Step.LOADED, data: data })
+          this.setState({ frequency: json.series.docs[0]['@frequency'], step: Step.LOADED, data: data })  
       })
       .catch(err => {
         if (err.name !== 'AbortError')
@@ -34,9 +55,28 @@ export default class CausesOfDeath extends React.Component {
       });
   }
 
+  setYear(addValue){
+    if((this.state.year > 2011 && addValue === -1 ) || (addValue === 1 && this.state.year < 2016)){
+      let newYear = this.state.year + addValue;
+      this.setState({ year : newYear});
+      this.organizeTierList(this.state.year);
+    }
+  }
+
+  organizeTierList(chosenDate){
+    arrayTierList = [];
+    for(let i = 0; i < this.state.data.length; i++){
+      if(this.state.data[i].deaths[chosenDate-2011].value !== "NA"){
+        countryTierList = this.state.data[i].country;
+        arrayTierList[i] = {'country' : countryTierList, 'value' :(this.state.data[i].deaths[chosenDate-2011].value)};
+      }
+    }
+    arrayTierList = arrayTierList.sort(function(a, b) {
+      return parseFloat(b.value) - parseFloat(a.value);  
+    });
+  }
 
   render() {
-    //console.log(this.state.data);
   switch (this.state.step) {
     case Step.LOADING: return (
       <div className="CausesOfDeath">
@@ -46,7 +86,17 @@ export default class CausesOfDeath extends React.Component {
 
     case Step.LOADED: return (
       <div className="CausesOfDeath">
-        <p>TOP CAUSES OF DEATH</p>
+        <h3>TOP EU COUNTRIES : crude deaths</h3>
+        <p>{this.state.year}</p>
+        <CountryButton onClick={e => this.setYear(-1)} name="<"></CountryButton>
+        <CountryButton onClick={e => this.setYear(1)} name=">"></CountryButton>
+        
+        <p>1. { arrayTierList[0].country }</p> 
+        <p>2. { arrayTierList[1].country }</p>
+        <p>3. { arrayTierList[2].country }</p>
+        <p>4. { arrayTierList[3].country }</p>   
+        <p>5. { arrayTierList[4].country }</p>     
+
       </div>
     )
 
@@ -56,7 +106,5 @@ export default class CausesOfDeath extends React.Component {
       </div>
     )}
   }
-
-
   requestController = new AbortController();
 }
