93318658622847765c58eaa644fcd8ef9addc4f0
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2020-04-24 18:48:50 +0200

    :construction: Add bottom nav on mobile

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,34 @@ import HellsDoor from './HellsDoor/HellsDoor';
 import Statishlag from './Statishlag/Statishlag';
 import Contact from './Contact/Contact';
 import SideNav from 'components/SideNav/SideNav';
+import BottomNav from './BottomNav/BottomNav';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { screenWidth: null };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+  
+  componentDidMount() {
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions)
+  }
+
+  updateWindowDimensions() {
+    this.setState({ screenWidth: window.innerWidth });
+  }
+
   render() {
     return (
       <BrowserRouter>
-        <SideNav></SideNav>
+        { (this.state.screenWidth > 1000 ) 
+        ? <SideNav></SideNav> 
+        : <BottomNav></BottomNav> 
+        }
         <Switch>
           <Route exact path={"/HellsDoor"} component={HellsDoor} />
           <Route exact path={"/Statishlag"} component={Statishlag} />
---
+++ src/components/BottomNav/BottomNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './BottomNav.css';
+
+export default class BottomNav extends React.Component {
+  render() {
+    return (
+      <div className="BottomNav">
+          <p>BottomNav</p>
+      </div >
+    )
+  }
+}
