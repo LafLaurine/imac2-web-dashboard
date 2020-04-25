4601133e994e4473d08d770ed0bbf63282e76a2e
LafLaurine <laurine.lafontaine@outlook.fr>
2020-04-25 17:43:49 +0200

    :wastebasket: Remove button on Poverty

--- src/components/Statishlag/Poverty/Poverty.js
+++ src/components/Statishlag/Poverty/Poverty.js
@@ -18,26 +18,14 @@ export default class Poverty extends React.Component {
       frequency: '',
       step: Step.LOADING,
       data: [],
-      indexCountry: 0
+      countryIndex: 0,
     }
-    this.updateCountry = this.updateCountry.bind(this);
-    this.updateCountry = this.updateCountry.bind(this);
+    this.changeCountry = this.changeCountry.bind(this);
   }
 
   ////////////////////// React Hooks ////////////////////
 
   componentDidMount() {
-    this.retrieveData()
-  }
-
-  componentWillUnmount() {
-    if (this.state.step === Step.LOADING)
-      this.requestController.abort();
-  }
-
-  ////////////////////// Logic  ////////////////////
-
-  retrieveData() {
     fetch(Environment.dbNomicsUrl + 'v22/series/Eurostat/ilc_peps02?limit=1000&offset=0&q=poverty&observations=1&align_periods=1&dimensions=%7B%7D',
       { method: 'GET', signal: this.requestController.signal })
       .then(res => { return res.json() })
@@ -55,18 +43,28 @@ export default class Poverty extends React.Component {
       });
   }
 
+  componentWillUnmount() {
+    if (this.state.step === Step.LOADING)
+      this.requestController.abort();
+  }
+
+  ////////////////////// Logic  ////////////////////
+
   /**
    * @brief Change country when user click on button and get the associated data
    */
-  updateCountry() {
-    this.setState({ indexCountry: (Math.floor(Math.random() * this.state.data.length)) });
-    this.retrieveData();
+  changeCountry() {
+    const maxIndex = this.state.data.length - 1;
+    if (this.state.countryIndex >= maxIndex)
+      this.setState({ countryIndex: 0 });
+    else
+      this.setState({ countryIndex: this.state.countryIndex + 1 });
   }
 
   ////////////////////// Render ////////////////////
 
   renderGraph() {
-    return <PovertyChart data={this.state.data[this.state.indexCountry]}></PovertyChart>
+    return <PovertyChart data={this.state.data[this.state.countryIndex]}></PovertyChart>
   }
 
   render() {
@@ -81,8 +79,7 @@ export default class Poverty extends React.Component {
         <div className="Poverty">
           <div className="container">
             <div className="element">
-              <p>Poverty in {this.state.data[this.state.indexCountry].country} ?</p>
-              <Button onClick={e => this.updateCountry()} name="Another country"></Button>
+              <p>Poverty in <span className="settings" onClick={this.changeCountry}> {this.state.data[this.state.countryIndex].country} </span>?</p>
             </div>
             {this.renderGraph()}
           </div>
