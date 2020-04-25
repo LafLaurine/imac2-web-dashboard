e83fac81fb32c72a19f646adacc2748079d7787e
Mathilde <mathildedumoulin54@gmail.com>
2020-04-25 18:59:31 +0200

    :sparkles: CSS on Crude Deaths :sparkles:

--- src/components/Statishlag/CausesOfDeath/CausesOfDeath.js
+++ src/components/Statishlag/CausesOfDeath/CausesOfDeath.js
@@ -6,21 +6,6 @@ import Environment from 'environment';
 import Step from 'shared/Step';
 import Loading from 'shared/Loading/Loading';
 
-/*
- let arrayTierList = {
-  0: { country : "Lithuania"},
-  1: { country : "Romania"},
-  2: { country : "Latvia"},
-  3: { country : "Portugal"},
-  4: { country : "Croatia"},
-  5: { country : "Bulgaria"},
-  6: { country : "Estonia"},
-  7: { country : "France"},
-  8: { country : "Finland"},
-  9: { country : "Poland"}
-};
-*/
-
 export default class CausesOfDeath extends React.Component {
 
 
@@ -66,7 +51,7 @@ export default class CausesOfDeath extends React.Component {
       .then(json => {
         const data = json.series.docs
           .filter(geo => this.state.countries.includes(geo.dimensions.geo))
-          .filter(values => values.value[0] < 4)
+          .filter(values => values.value[0] > 4)
           .map(country => ({
             'country': json.dataset.dimensions_values_labels.geo[country.dimensions.geo],
             'deaths': country.period.map((date, index) => ({ 'date': date, 'value': country.value[index] })),
@@ -93,7 +78,7 @@ export default class CausesOfDeath extends React.Component {
     let arrayTierList = [];
     for (let i = 0; i < this.state.data.length; i++) {
       if (this.state.data[i].deaths[chosenDate - 2011].value !== "NA") {
-        arrayTierList[i] = { 'country': this.state.data[i].country, 'value': (this.state.data[i].deaths[chosenDate - 2011].value) };
+        arrayTierList[i] = { 'country': this.state.data[i].country, 'value': (Math.floor(this.state.data[i].deaths[chosenDate - 2011].value)) };
       }
     }
     arrayTierList = arrayTierList.sort(function (a, b) {
@@ -117,14 +102,22 @@ export default class CausesOfDeath extends React.Component {
 
         <div className="CausesOfDeath">
 
-          <h3>TOP EU COUNTRIES : crude deaths</h3>
-          <p>{this.state.year}</p>
-          <Button onClick={e => this.setYear(-1)} name="<" ></Button>
-          <Button onClick={e => this.setYear(1)} name=">" ></Button>
+          <div id = "title">
+            <h3>Top EU countries : crude deaths edition</h3>
+            <h4>Including accidents, poisoning, rare diseases and more ! </h4>
+          </div>
 
+          <div id = "selectYear">
+            <Button onClick={e => this.setYear(-1)} name="<" ></Button>
+            <p id = "year">{this.state.year}</p>
+            <Button onClick={e => this.setYear(1)} name=">" ></Button>
+          </div>
+
+          <div id = "tierList">
           {Object.keys(this.state.tierList).map((item, index) => {
-            return <p className="tierList-input" key={index}> {index + 1}. {this.state.tierList[index].country}</p>
+            return <p className="tierListElem" key={index}> {index + 1}. {this.state.tierList[index].country} </p>
           })}
+          </div>
 
 
         </div>
