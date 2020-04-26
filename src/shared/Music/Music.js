59f3b443f5de261a513bec3f86f220ae2d3d362e
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2020-04-26 18:20:30 +0200

    :lipstick: Change loading screen

--- src/shared/Music/Music.js
+++ src/shared/Music/Music.js
@@ -3,14 +3,13 @@ import './Music.css'
 import hellMusic from './music/hellMusic.mp3'
 
 export default class Music extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <audio controls loop className="audio-element">
-                    <source src={hellMusic}></source>
-                </audio>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Music">
+        <audio controls autoPlay loop className="audio-element">
+          <source src={hellMusic}></source>
+        </audio>
+      </div>
+    );
+  }
 }
